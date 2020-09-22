9376fd4671dd98b7243924c5e5ff1d48fa99f2a3
antti <antti.hyppola@hotmail.com>
2020-09-22 18:32:44 +0100

    Updates

---
+++ precache-manifest.ecb9557e3cb499d0397e4fda3676e081.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "829194987d3e56ed39d6dc9811868c84",
+    "url": "/quiz-game/index.html"
+  },
+  {
+    "revision": "84983f0b652e4155c05c",
+    "url": "/quiz-game/static/js/2.bf20ed47.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quiz-game/static/js/2.bf20ed47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2968fa593dcf70d17cdc",
+    "url": "/quiz-game/static/js/main.f5fc8aed.chunk.js"
+  },
+  {
+    "revision": "7b35795b0c23e32b4560",
+    "url": "/quiz-game/static/js/runtime-main.0ff60c08.js"
+  },
+  {
+    "revision": "544ee49de038947b29b11fd8fcae1e30",
+    "url": "/quiz-game/static/media/tetris.544ee49d.jpg"
+  }
+]);
